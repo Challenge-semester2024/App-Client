992ac461ea6cf000e989823bf58d59b7b7d6e420
whitecity01 <kh081104@gmail.com>
2024-05-06 19:53:48 +0900

    feat: mainPage 문구 추가

--- child_care_app/lib/screens/main_page.dart
+++ child_care_app/lib/screens/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:child_care_app/screens/login_page.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatelessWidget {
@@ -9,38 +10,82 @@ class MainPage extends StatelessWidget {
       body: Align(
         alignment: Alignment.center,
         child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.6,
+          width: MediaQuery.of(context).size.width * 0.9,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 50.0),
-              Image.asset(
-                // 로고
-                'assets/logo.png',
-                width: 261,
-                height: 220,
+              const SizedBox(height: 180.0),
+              const Text(
+                "아지트에 오신 것을 환영합니다.\n로그인 후 아래 기능들을 이용해보세요.",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
               ),
+              const SizedBox(height: 60.0),
+              const Text(
+                "전국 보육원 정보들을 한 눈에",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 18.0),
+              const Text(
+                "가장 투명한 봉사/후원 내역",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 18.0),
+              const Text(
+                "관심 보육원 즐겨찾기",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 18.0),
+              const Text(
+                "봉사 일정 문자 알림",
+                style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 150.0),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: const Color.fromARGB(255, 255, 0, 0),
+                    backgroundColor:
+                        const Color.fromARGB(255, 0, 0, 0), // 텍스트 색을 흰색으로 설정
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(80, 8, 80, 8),
+                    child: const Text(
+                      "로그인 하기",
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          fontSize: 15),
+                    ),
+                  ),
+                ),
+              ),
+              Center(
+                child: TextButton(
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginPage()),
+                    );
+                  },
+                  style: TextButton.styleFrom(
+                    foregroundColor:
+                        const Color.fromARGB(255, 0, 0, 0), // 텍스트 색을 검은색으로 설정
+                  ),
+                  child: const Text(
+                    "회원가입",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                ),
+              )
             ],
           ),
         ),
       ),
     );
   }
-}
-
-ElevatedButton buildButton(BuildContext context, String text, Widget page) {
-  return ElevatedButton(
-    onPressed: () {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => page),
-      );
-    },
-    child: Container(
-      padding: const EdgeInsets.fromLTRB(40, 35, 40, 35),
-      child: Text(
-        text,
-        style: const TextStyle(color: Colors.black, fontSize: 25),
-      ),
-    ),
-  );
 }
